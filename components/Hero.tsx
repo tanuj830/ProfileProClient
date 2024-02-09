0720ceccbeed2cbd4260fa4aa12de54d8f087967
tanuj <tanujbhatt644@gmail.com>
2024-02-09 21:08:21 +0530

    first commit

---
+++ components/Hero.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Lines from "./Lines";
+
+const Hero = () => {
+  return (
+    <div className="relative h-screen grid grid-rows-6 gap">
+      <Lines />
+    </div>
+  );
+};
+
+export default Hero;
---
+++ components/Lines.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Lines = () => {
+  return (
+    <div className="lines ">
+      <div className="abslines">
+        <div className="linefull"></div>
+        <div className="linefull20"></div>
+        <div className="linefull20"></div>
+        <div className="linefull"></div>
+        <div className="linefull"></div>
+        <div className="linefull20"></div>
+        <div className="linefull20"></div>
+        <div className="linefull"></div>
+        <div className="linefull"></div>
+        <div className="linefull20"></div>
+        <div className="linefull20"></div>
+        <div className="linefull"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Lines;
