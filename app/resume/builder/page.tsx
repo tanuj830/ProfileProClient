97a7ef907bb9c237251c0e527576c5323d316495
tanuj <tanujbhatt644@gmail.com>
2024-02-21 13:16:17 +0530

    safe reloading implemented

--- app/resume/builder/page.tsx
+++ app/resume/builder/page.tsx
@@ -88,6 +88,10 @@ const page = () => {
       setProjectPageActive(true);
     }
   };
+
+  window.onbeforeunload = function () {
+    return "Are you really want to perform the action? \n This action will reset your all states.";
+  };
   return (
     <div className="flex flex-col-reverse lg:flex-row gap-5">
       <div className="flex  flex-col w-full lg:w-[50%]">
